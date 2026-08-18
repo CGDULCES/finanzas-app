bf15d3c0260a3047896c1b3e431e31a4c10e6fb3
CGDULCES <alcidesbenega05@gmail.com>
2026-08-18 10:16:17 -0300

    Update Supabase configuration values
    Updated Supabase configuration with new URL and anon key.

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // =====================================================================
 
 const CONFIG = {
-  SUPABASE_URL: "https://TU-PROYECTO.supabase.co",
-  SUPABASE_ANON_KEY: "TU-ANON-PUBLIC-KEY",
+  SUPABASE_URL: "https://eefvzaifkcnoartezkte.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "sb_publishable_AduCWjzZEYptm24qvQ5atQ_ZiNrOEj1",
 
   // Podés cambiar los nombres visibles de cada sección acá.
   SECTIONS: {
